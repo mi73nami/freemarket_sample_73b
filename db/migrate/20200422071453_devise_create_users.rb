cabd71ce6ded03c08e067c498a6aade8945e8116
mitsuhiko-y <realreborn1021@gmail.com>
2020-04-23 11:15:52 +0900

    devise_migrationfile_edit_db:migrate_by_yamazaki

--- db/migrate/20200422071453_devise_create_users.rb
+++ db/migrate/20200422071453_devise_create_users.rb
@@ -4,9 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
+      t.string :nickname,               null: false, default: ""
+      t.string :email,                  null: false, default: "", uniqueness: true
+      t.string :encrypted_password,     null: false, default: ""
+      t.string :last_name,              null: false, default: ""
+      t.string :first_name,             null: false, default: ""
+      t.string :ruby_last_name,         null: false, default: ""
+      t.string :ruby_first_name,        null: false, default: ""
+      t.string :birthdate,              null: false, default: ""
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
