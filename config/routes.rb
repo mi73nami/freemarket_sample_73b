af3d13a37e1e3083a36c251e709f08368df9a266
mitsuhiko-y <realreborn1021@gmail.com>
2020-04-25 20:35:15 +0900

    update_routes-rb
    -what
    ウィザード形式ユーザ登録実装の為の追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
-
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
+    get  'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
   end
 
   root to: "home#index"
