e136d8446b638c35162fb685e1d0bf8fdaa958d6
Aokitakeshi0721 <yahpan@gmail.com>
2020-04-25 13:35:45 +0900

    ブランチ作成のため

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_204419) do
+ActiveRecord::Schema.define(version: 2020_04_24_204541) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
+    t.string "name"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
