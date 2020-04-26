ddbf17e4361c786b1c02e5743d0c4b739cb9c820
yutakahashi1 <federeryuukun1@gmail.com>
2020-04-26 15:10:10 +0900

    add category function
    ## What
    - category.rb  "use active hash"
    - gem active hash
    - category.js
    - index.json.builder
    - home.controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,15 @@ class HomeController < ApplicationController
     @products = Product.all
     @parents = Category.where(ancestry: nil)
     @categories = Category.where(ancestry: params[:id])
+
+    @main_categories = Category.where(sub: '0')
+    @sub_categories = Category.where(sub: params[:parent], sub_sub: '0')
+    @sub_sub_categories = Category.where(sub: params[:child], sub_sub: params[:grandChild])
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  
   end
 
 end
