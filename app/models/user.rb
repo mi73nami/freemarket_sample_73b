2bdf9a793037802a408b19f46c4cd7d89c15668d
mitsuhiko-y <realreborn1021@gmail.com>
2020-04-26 15:08:04 +0900

    update_ユーザ登録エラーメッセージ環境調性
    -what
    PW文字数制限設定、エラーメッセ和約、確認用PW欄追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable,  :validatable
+         :recoverable, :rememberable,  :validatable, password_length: 7..128
 
-  validates :nickname, :email, :password, :last_name, :first_name, :ruby_last_name, :ruby_first_name, :birthdate, presence: true
+  validates :nickname, :password, :email, :last_name, :first_name, :ruby_last_name, :ruby_first_name, :birthdate, presence: true
   has_many :products
   has_one :ship_address
 end
