b3658cfc859d2fa4c84d02b9f9b3dd0965325d9e
yutakahashi1 <federeryuukun1@gmail.com>
2020-04-27 20:40:23 +0900

    update category function
    ## What
    - category/index.html.haml&scss
    - home/index.html.haml&scss
    - categories.controller
    ## Why
    - to complete the category function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,18 @@
 class CategoriesController < ApplicationController
+  before_action :set_parents
+
+  def index
+  end
+
+  def show
+    @products = Product.joins(:categories).where(categories: {id: params[:id]})
+    @category = Category.find(params[:id])
+  end
+
+  private
+  def set_parents
+    @category_parent = Category.where(ancestry: nil) 
+  end
+
 end
+
