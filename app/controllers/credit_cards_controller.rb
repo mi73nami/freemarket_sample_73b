f1a5254c0523ae1ac3a8e4b852c934ebbfa2366a
mitsuhiko-y <realreborn1021@gmail.com>
2020-04-29 16:15:08 +0900

    update_creditcards_controller.rb編集
    -what
    クレジットカードの登録のためnew,create関連を追記

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,26 +1,39 @@
 class CreditCardsController < ApplicationController
+  require "payjp"
+  before_action :set_card
 
-  def new
+  def new # カードの登録画面。送信ボタンを押すとcreateアクションへ。
+    card = Card.where(user_id: current_user.id).first
+    redirect_to action: "index" if card.present?
   end
 
-  def create
-    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+ # indexアクションはここでは省略
 
-    if params["payjp_token"].blank?
-      redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
+  def create #PayjpとCardのデータベースを作成
+    Payjp.api_key = 'API_KEY'
+
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
     else
+      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
       customer = Payjp::Customer.create(
+        description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
         email: current_user.email,
-        card: params["payjp_token"],
-        metadata: {user_id: current_user.id}
+        card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
+        metadata: {user_id: current_user.id} # 無くてもOK。
       )
-      @card = CreditCard.new(user_id: current_user.id, token: payjp_token)
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to user_path(current_user)
+        redirect_to action: "index"
       else
-        render :new
+        redirect_to action: "create"
       end
     end
   end
 
+  private
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+  end
 end
