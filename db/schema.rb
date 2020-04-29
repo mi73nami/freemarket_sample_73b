63c12cae8c8e3d405c5481a377f7bf0b8a324ef2
mitsuhiko-y <realreborn1021@gmail.com>
2020-04-29 16:49:05 +0900

    update_creditcards/new.html.haml
    -what
    クレジットカード登録ビュー編集、
    クレジットカードコントローラ編集

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_04_29_064830) do
-ActiveRecord::Schema.define(version: 2020_04_29_012557) do
-
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
